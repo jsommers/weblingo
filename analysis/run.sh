8d221cea7e5871a035b5d1e85f7dc9fe676270d9
Joel Sommers <jsommers@colgate.edu>
2017-09-21 09:10:43 -0400

    mod run.sh for prod spark; fix requirements

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
-~/Downloads/spark-2.2.0/bin/spark-submit spark_analy.py analyze=spark_lang_extract dir=`pwd`/testdata pattern='GB_langpref_0000*' out='GB_langpref_summary.json'
-
+#~/Downloads/spark-2.2.0/bin/spark-submit spark_analy.py analyze=spark_lang_extract dir=`pwd`/testdata pattern='GB_langpref_0000*' out='GB_langpref_summary.json'
 # ~/Downloads/spark-2.2.0/bin/spark-submit spark_analy.py analyze=errs dir=`pwd`/testdata pattern='GB_langpref_*' out=errs.txt
+
+spark-submit --master local[8] spark_analy.py analyze=errs dir=/data/weblingo/GB pattern='GB_langpref_??????' out='GB_langpref_errs.txt'
+
+spark-submit --master local[8] spark_analy.py analyze=spark_lang_extract dir=/data/weblingo/GB pattern='GB_langpref_??????' out='GB_langpref_summary.json'
+
+spark-submit --master local[8] spark_analy.py analyze=primarylang dir=/data/weblingo/GB pattern='GB_langpref_summary*' out='GB_langpref_summary.txt'
+
