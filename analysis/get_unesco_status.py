d8adcbba5a4e24b7f234f8678bce6338f6344443
Joel Sommers <jsommers@colgate.edu>
2017-10-12 20:07:36 -0400

    add endangered lang script

---
+++ analysis/get_unesco_status.py
@@ -0,0 +1,71 @@
+import sys
+import xml.etree.ElementTree as ET
+from glob import glob
+import json
+from collections import defaultdict
+import langtags
+
+
+lcode_remap = {}
+with open('iso-639-3.tab.txt') as infile:
+    infile.readline() # discard header line
+    for line in infile:
+        fields = line.split('\t')
+        iso3 = fields[0].strip()
+        iso1 = fields[3].strip()
+        if not iso1:
+            iso1 = iso3
+        lcode_remap[iso3] = iso1
+
+
+tree = ET.parse('unesco_atlas_languages_limited_dataset.xml')
+root = tree.getroot()
+
+status = {}
+
+for child in root:
+    # print(child.tag)
+    endanger = ''
+    t = None
+    xname = None
+    for subchild in child:
+        if subchild.tag == 'Degree_of_endangerment':
+            endanger = subchild.text 
+        elif subchild.tag == 'ISO639-3_codes':
+            if subchild.text is None:
+                continue
+
+            codes = [ t.strip() for t in subchild.text.split(',') ]
+            for c in codes:
+                remap = lcode_remap.get(c, '')
+                if remap:
+                    t = langtags.Tag(remap)
+        elif subchild.tag == 'Name_in_English':
+            xname = subchild.text.strip()
+
+    if t and endanger:
+        status[t.language.subtag] = (endanger, xname)
+
+
+def getdata(f):
+    with open(f) as infile:
+        lines = 6
+        while lines > 0:
+            nextline = infile.readline()
+            lines -= 1
+    return json.loads(nextline)
+
+for f in glob("testdata/??_default_alllang.txt"):
+    endangerdict = defaultdict(list)
+    d = getdata(f)
+    for tag, ct in d.items():
+        if tag in status:
+            stat, name = status[tag]
+            endangerdict[stat].append((tag, ct, name))
+
+    print(f)
+    for statustype, dlist in endangerdict.items():
+        dlist.sort(key=lambda t: t[1], reverse=True)
+        print(statustype, dlist[:10])
+
+
